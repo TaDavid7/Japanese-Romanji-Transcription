f92d657980b23fc93e52c58525c93e7de4d2dc3e
David Tanioka <dtanioka7@gmail.com>
2026-07-24 23:39:55 -0700

    comments for eval

---
+++ 4. mp3Conversion_language_romanji/routes/eval_readings.py
@@ -0,0 +1,4 @@
+#Need to compare
+#raw audio -> kanji (whispher) -> romanji (mecab)
+#raw audio -> kanji (whispher) -> romanji (pykaksi)
+#raw audio -> hiragana (whispher + prompt) -> romanji (jaconv)
